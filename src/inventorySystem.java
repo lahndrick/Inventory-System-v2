7f4be4921299bd9565c98bd6e731d384b67de576
Lahndrick <lahndrickhendricks2@gmail.com>
2022-11-22 21:43:48 +1300

    gui updates

--- src/inventorySystem.java
+++ src/inventorySystem.java
@@ -64,17 +64,18 @@ public class inventorySystem {
         frame.getContentPane().add(searchItemTextField);
 
         //TODO update sizes for each component
-        skuCodeTextField.setSize(50,50);
-        barcodeTextField.setSize(50,50);
-        locationTextField.setSize(50,50);
-        quantityTextField.setSize(50,50);
-        addItemButton.setSize(50,50);
+        skuCodeTextField.setSize(120,40);
+        barcodeTextField.setSize(120,40);
+        locationTextField.setSize(120,40);
+        quantityTextField.setSize(120,40);
+        addItemButton.setSize(120,40);
+
         removeItemButton.setSize(50,50);
         searchItemButton.setSize(50,50);
         tableNameTextField.setSize(50,50);
         removeTableButton.setSize(50,50);
         addTableButton.setSize(50,50);
-        allItemsScrollPane.setSize(50,50);
+        allItemsScrollPane.setSize(500,190);
         allItemsTextField.setSize(50,50);
         foundItemsScrollPane.setSize(50,50);
         foundItemsTextField.setSize(50,50);
@@ -82,23 +83,29 @@ public class inventorySystem {
 
         //TODO update locations to appropriate places
         skuCodeTextField.setLocation(0,0);
-        barcodeTextField.setLocation(60,0);
-        locationTextField.setLocation(120,0);
-        quantityTextField.setLocation(180,0);
-        addItemButton.setLocation(240,0);
+        barcodeTextField.setLocation(0,50);
+        locationTextField.setLocation(0,100);
+        quantityTextField.setLocation(0,150);
+        addItemButton.setLocation(0,200);
+        allItemsScrollPane.setLocation(130,0);
+        allItemsScrollPane.add(allItemsTextField);
+/* 
         removeItemButton.setLocation(300,0);
         searchItemButton.setLocation(360,0);
         tableNameTextField.setLocation(420,0);
         removeTableButton.setLocation(480,0);
         addTableButton.setLocation(540,0);
-        allItemsScrollPane.setLocation(600,100);
-        allItemsScrollPane.add(allItemsTextField);
         foundItemsScrollPane.setLocation(720,0);
         foundItemsTextField.setLocation(780,0);
         searchItemTextField.setLocation(840,0);
+*/
+        barcodeTextField.setToolTipText("barcode");
+        skuCodeTextField.setToolTipText("sku code");
+        locationTextField.setToolTipText("location");
+        quantityTextField.setToolTipText("quantity");
+        addItemButton.setText("Add item");
 
-
-        frame.setSize(1000, 800);
+        frame.setSize(800, 500);
         frame.setTitle("Inventory System");
         frame.setLocation(100, 100);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
